dc7e89eda7e80bd606950ea4dba3385957341555
oott123 <git@public.oott123.com>
2026-01-05 00:06:09 +0800

    feat: Add Nix flake for a PyQt FHS development environment with virtualenv support, including a `.gitignore` for build artifacts.

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "PyQt FHS environment with .venv support";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = (pkgs.buildFHSEnv {
+          name = "pyqt-fhs";
+          targetPkgs = pkgs: with pkgs; [
+            # Python
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+            
+            # Qt basics
+            libsForQt5.qt5.qtbase
+            libsForQt5.qt5.qtwayland
+            
+            # Common libraries for python packages / Qt
+            glib
+            libGL
+            libxkbcommon
+            fontconfig
+            freetype
+            dbus
+            zlib
+            gcc
+            gnumake
+            
+            # X11 libs (often needed even on Wayland for XWayland or callbacks)
+            xorg.libX11
+            xorg.libXi
+            xorg.libXext
+            xorg.libXrender
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXfixes
+          ];
+          
+          # Script to run when entering the FHS environment
+          runScript = pkgs.writeScript "init-fhs.sh" ''
+            # Check if .venv exists, if not create it
+            if [ ! -d ".venv" ]; then
+              echo "Creating virtual environment (.venv)..."
+              python -m venv .venv
+            fi
+            
+            # Prepare the activation command
+            # We use a custom bashrc to source the activate script and then the user's bashrc
+            
+            echo "Activating .venv..."
+            source .venv/bin/activate
+            
+            # Launch bash with the venv activated
+            # We inherit the environment variables from the source above
+            exec bash
+          '';
+        }).env;
+      }
+    );
+}
