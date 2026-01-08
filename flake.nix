f12d78e50c15e41454d035f2d48a31b75d75eecc
oott123 <git@public.oott123.com>
2026-01-08 21:41:47 +0800

    fix: qml deps

--- flake.nix
+++ flake.nix
@@ -22,18 +22,30 @@
         ]);
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = [
+          ];
+
+          packages = [
             pythonEnv
-            
-            # Qt native deps
+
+            libsForQt5.qt5.wrapQtAppsHook
             libsForQt5.qt5.qtbase
             libsForQt5.qt5.qtwayland
+            libsForQt5.qt5.qtdeclarative
+            libsForQt5.qt5.qtquickcontrols
+            libsForQt5.qt5.qtquickcontrols2
           ];
           
           shellHook = ''
+            setQtEnvironment=$(mktemp --suffix .setQtEnvironment)
+            makeWrapper "/bin/sh" "$setQtEnvironment" "''${qtWrapperArgs[@]}"
+            export QT_PLUGIN_PATH=$("$setQtEnvironment" -c "echo $QT_PLUGIN_PATH")
+            export QML2_IMPORT_PATH=$("$setQtEnvironment" -c "echo $QML2_IMPORT_PATH")
+
             export PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}
             python -V
+            echo "nix devshell rebuild success! You can use new dependencies now."
           '';
         };
       }
